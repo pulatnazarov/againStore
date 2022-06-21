fe447c64f6810e3ec06dee66f2dbd210972ab729
shahTeam <shahzodasturchi@gmail.com>
2022-06-21 15:28:20 +0500

    add customer shop

--- store/store.go
+++ store/store.go
@@ -1,34 +1,72 @@
 package store
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/againStore/inventory"
+)
 
 type Store struct {
 }
 
 func (s Store) Sell() {
+	for {
 	end:
-	fmt.Printf(`
+		fmt.Printf(`
 	1-Shop
 	2-List
 	3-Exit
 	`)
+		sentence, er := inventory.RightInput()
+		if er != nil {
+			fmt.Println(" ", er)
+			goto end
+		}
 
+		sentence = sentence[:len(sentence)-1]
+		x, er := strconv.Atoi(sentence)
+		if er != nil {
+			fmt.Println(er)
+			goto end
+		}
+		if x > 2 || x < 1 {
+			fmt.Println(er)
+			goto end
+		}
+		if x == 1 {
+			s.Shop()
+		} else if x == 2 {
+
+		}
+	}
+}
+func (s Store) Shop() {
+	fmt.Println("Mahsulot nomini kiriting: ")
+	sentence, er := inventory.RightInput()
+	if er != nil {
+		fmt.Println(er)
+	}
+	sentence = sentence[:len(sentence)-1]
+start:
+	fmt.Println("Enter product qountity(only number): ")
+	quantity, er := inventory.RightInput()
+	if er != nil {
+		fmt.Println(er)
+	}
+	quantity = quantity[:len(quantity)-1]
+	x, er := strconv.Atoi(quantity)
+	if er != nil {
+		fmt.Println(er)
+		goto start
+	}
+	if x <= 0 {
+		fmt.Println("enter positive number")
+		goto start
+	}
+	
 }
 
-// sentence, er := RightInput()
-// if er != nil {
-// 	fmt.Println(" ", er)
-// 	goto end
-// }
+func (s Store) List() {
 
-// sentence = sentence[:len(sentence-1)]
-// x, er := strconv.Atoi(sentence)
-// if er != nil {
-// 	fmt.Println(er)
-// 	goto end
-// }
-// if x>2 || x<1 {
-// 	fmt.Println(er)
-// }else if x == 1{
-// 	return
-// }
+}
