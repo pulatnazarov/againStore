38244b3471d744e2ebd3d29a9198dca0b38e5f36
narimboyevsoburjon@gmail.com <narimboyevsoburjon@gmail.com>
2022-06-21 16:14:31 +0500

    data Sell and Dell func created

--- data/file-inventory.go
+++ data/file-inventory.go
@@ -31,16 +31,46 @@ func Add(name string, n, pr, op uint64) (bool, error) {
 	if s == 0 {
 		list = append(list, product.Product{Name: name, Quantity: n, Price: pr, OriginPrice: op})
 	}
+	write(list)
+	return true, nil
+}
+func write(list product.List) {
 	js, err := json.MarshalIndent(list, "", "	")
 	if err != nil {
-		return false, err
+		panic(err)
 	}
 	err = os.WriteFile("data/data.json", js, 0600)
 	if err != nil {
-		return false, err
+		panic(err)
 	}
-	return true, nil
 }
-func Sell() {
-
+func Sell(name string, q uint64) (uint64, bool) {
+	list := List()
+	for i, p := range list {
+		if p.Name == name {
+			if p.Quantity-q > 0 {
+				list[i].Quantity -= q
+				write(list)
+				return p.Price, true
+			} else if p.Quantity-q < 0 {
+				return 0, false
+			}
+			del(i, &list)
+			write(list)
+			return p.Price, true
+		}
+	}
+	return 0, false
+}
+func del(i int, l *product.List) {
+	*l = append((*l)[:i], (*l)[i+1:]...)
+}
+func Dell(name string) {
+	list := List()
+	for i, p := range list {
+		if p.Name == name {
+			del(i, &list)
+		}
+	}
+	write(list)
 }
